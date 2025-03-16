36b7bf04cc2af35c0cfd4d7e2b934c46950b161e
Mikhael Djajapermana <mikhael.djajapermana@gmail.com>
2025-03-16 19:33:09 +0100

    add Cifar10RunConfig

--- ofa/imagenet_classification/run_manager/run_config.py
+++ ofa/imagenet_classification/run_manager/run_config.py
@@ -3,7 +3,7 @@
 # International Conference on Learning Representations (ICLR), 2020.
 
 from ofa.utils import calc_learning_rate, build_optimizer
-from ofa.imagenet_classification.data_providers import ImagenetDataProvider
+from ofa.imagenet_classification.data_providers import ImagenetDataProvider, Cifar10DataProvider
 
 __all__ = ["RunConfig", "ImagenetRunConfig", "DistributedImageNetRunConfig"]
 
@@ -118,6 +118,76 @@ class RunConfig:
             self.weight_decay,
             self.no_decay_keys,
         )
+    
+
+class Cifar10RunConfig(RunConfig):
+    def __init__(
+        self,
+        n_epochs=150,
+        init_lr=0.05,
+        lr_schedule_type="cosine",
+        lr_schedule_param=None,
+        dataset="cifar10",
+        train_batch_size=64,
+        test_batch_size=64,
+        valid_size=None,
+        opt_type="sgd",
+        opt_param=None,
+        weight_decay=4e-5,
+        label_smoothing=0.1,
+        no_decay_keys=None,
+        mixup_alpha=None,
+        model_init="he_fout",
+        validation_frequency=1,
+        print_frequency=10,
+        n_worker=4,
+        resize_scale=0.08,
+        distort_color=None,
+        image_size=32,
+        **kwargs
+    ):
+        super(Cifar10RunConfig, self).__init__(
+            n_epochs,
+            init_lr,
+            lr_schedule_type,
+            lr_schedule_param,
+            dataset,
+            train_batch_size,
+            test_batch_size,
+            valid_size,
+            opt_type,
+            opt_param,
+            weight_decay,
+            label_smoothing,
+            no_decay_keys,
+            mixup_alpha,
+            model_init,
+            validation_frequency,
+            print_frequency,
+        )
+
+        self.n_worker = n_worker
+        self.resize_scale = resize_scale
+        self.distort_color = distort_color
+        self.image_size = image_size
+
+    @property
+    def data_provider(self):
+        if self.__dict__.get("_data_provider", None) is None:
+            if self.dataset == Cifar10DataProvider.name():
+                DataProviderClass = Cifar10DataProvider
+            else:
+                raise NotImplementedError
+            self.__dict__["_data_provider"] = DataProviderClass(
+                train_batch_size=self.train_batch_size,
+                test_batch_size=self.test_batch_size,
+                valid_size=self.valid_size,
+                n_worker=self.n_worker,
+                resize_scale=self.resize_scale,
+                distort_color=self.distort_color,
+                image_size=self.image_size,
+            )
+        return self.__dict__["_data_provider"]
 
 
 class ImagenetRunConfig(RunConfig):
